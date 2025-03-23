118cdb2d68c242ec7c47d6f2be139a535272e025
ruben <rubenann@yahoo.com>
2025-03-23 01:32:07 +0100

    Title doc

--- assets/froala-editor.js
+++ assets/froala-editor.js
@@ -27,7 +27,7 @@ var editor = new FroalaEditor('#Questions_documentation_text', {
 window.onload = function() {
     setTimeout(function (){
         alert('kuku');
-        console.log($('div:contains("Unlicensed")'));
+        $('div:contains("Unlicensed")').parent().addClass('d-none');
     }, 500);
 }
 
